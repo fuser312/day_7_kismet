1917b049d0e503cfeafe1640ef25650bfdfa18ea
Sahil Singh <sahil.dogstar@gmail.com>
2019-10-09 21:17:11 +0530

    Finished Kismet App.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'story_brain.dart';
+import 'dart:io';
 
 void main() => runApp(MaterialApp(
       theme: ThemeData.dark(),
@@ -13,12 +15,17 @@ class KismetApp extends StatefulWidget {
 // TODO: Step 15 - Run your app and see if it works as expected
 class _KismetAppState extends State<KismetApp> {
   // TODO: Step 6 - Create a storyBrain object and use it to get story title, choice1 and choice 2 and show them on the app by using at appropriate locations
-
+  StoryBrain storyBrain = StoryBrain();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       //TODO: Step 1 - Add background.png to this Container as a background image. (Remember to add it to pubspec.yaml)
       body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/background.png")
+          )
+        ),
         padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 15.0),
         constraints: BoxConstraints.expand(),
         child: SafeArea(
@@ -29,7 +36,7 @@ class _KismetAppState extends State<KismetApp> {
                 flex: 12,
                 child: Center(
                   child: Text(
-                    'Story text will go here.',
+                    storyBrain.getStory(),
                     style: TextStyle(
                       fontSize: 25.0,
                     ),
@@ -38,16 +45,23 @@ class _KismetAppState extends State<KismetApp> {
               ),
               Expanded(
                 flex: 2,
-                child: FlatButton(
-                  onPressed: () {
-                    //Choice 1 made by user.
-                    //TODO: Step 9 - Call the nextStory() method from storyBrain and pass the number 1 as the choice made by the user, do the same for choice2 button and pass number 2 in that case
-                  },
-                  color: Colors.red,
-                  child: Text(
-                    'Choice 1',
-                    style: TextStyle(
-                      fontSize: 20.0,
+                child: Visibility(
+                  visible: storyBrain.buttonShouldBeVisible(),
+                  child: FlatButton(
+                    onPressed: () {
+                      //Choice 1 made by user.
+                      //TODO: Step 9 - Call the nextStory() method from storyBrain and pass the number 1 as the choice made by the user, do the same for choice2 button and pass number 2 in that case
+
+                      setState(() {
+                        storyBrain.nextStory(1);
+                      });
+                    },
+                    color: Colors.red,
+                    child: Text(
+                      storyBrain.getChoice1(),
+                      style: TextStyle(
+                        fontSize: 20.0,
+                      ),
                     ),
                   ),
                 ),
@@ -58,15 +72,21 @@ class _KismetAppState extends State<KismetApp> {
               Expanded(
                 flex: 2,
                 //TODO: Step 14 - Use a Flutter Visibility Widget to wrap this FlatButton and set the "visible" property of the Visibility Widget to equal the output from the buttonShouldBeVisible() method in the storyBrain.
-                child: FlatButton(
-                  onPressed: () {
-                    //Choice 2 made by user.
-                  },
-                  color: Colors.blue,
-                  child: Text(
-                    'Choice 2',
-                    style: TextStyle(
-                      fontSize: 20.0,
+                child: Visibility(
+                  visible: storyBrain.buttonShouldBeVisible(),
+                  child: FlatButton(
+                    onPressed: () {
+                      //Choice 2 made by user.
+                      setState(() {
+                        storyBrain.nextStory(2);
+                      });
+                    },
+                    color: Colors.blue,
+                    child: Text(
+                      storyBrain.getChoice2(),
+                      style: TextStyle(
+                        fontSize: 20.0,
+                      ),
                     ),
                   ),
                 ),
